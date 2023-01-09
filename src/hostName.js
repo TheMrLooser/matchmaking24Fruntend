de867b3b295db7bb069ce572cbd441e519849ab2
TheMrLooser <88968509+TheMrLooser@users.noreply.github.com>
2023-01-10 00:41:18 +0530

    host name changed

--- src/hostName.js
+++ src/hostName.js
@@ -1,2 +1,2 @@
 // export const HOST_NAME = "http://localhost:80";
-export const HOST_NAME = "https://founder-meet-backend.vercel.app";   
+export const HOST_NAME = "https://matchmaking24-backend.vercel.app";   
