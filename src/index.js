53256a4463cb3d29f79a1308f895c43584be811e
Ankush Kumar <ankushsoni7171@gmail.com>
2023-01-16 13:17:43 +0530

    updated rendring problem

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './CSS/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./CSS/index.css";
+import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
- 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <> 
+  <>
     <Auth0Provider
-    domain="dev-2wrhnz83wxyo8z5x.us.auth0.com"
-    clientId="AojwkiyZA7L6oUMtb6V9QA5c8ot39ZNO"
-    redirectUri={`https://matchmaking24.com/profile`} //   {`http://localhost:3000/`}
-     
-  >
-    <App />
-  </Auth0Provider>
+      domain="dev-2wrhnz83wxyo8z5x.us.auth0.com"
+      clientId="AojwkiyZA7L6oUMtb6V9QA5c8ot39ZNO"
+      redirectUri={`http://localhost:3000`} //{`https://matchmaking24.com/profile`} //   {`http://localhost:3000/`}
+    >
+      <App />
+    </Auth0Provider>
   </>
 );
- 
